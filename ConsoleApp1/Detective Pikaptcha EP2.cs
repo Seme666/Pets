9688bbb8534f92a0b5e42f094006a127a3c0c100
SemLozhkin <SemLozhkin@gmail.com>
2022-09-20 00:39:45 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Detective Pikaptcha EP2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Player
+{
+    void Main()
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]);
+        int height = int.Parse(inputs[1]);
+
+        int edgesCount = 0;
+        char freeCell = '0';
+        string[] lines = new string[height];
+
+
+        for (int i = 0; i < height; i++)
+        {
+            lines[i] = Console.ReadLine();
+            char[] line = lines[i].ToCharArray();
+            foreach (char c in line)
+            {
+                if (c == freeCell)
+                {
+                    freeCell = (char)edgesCount;
+                    edgesCount++;
+                }
+            }
+        }
+        string side = Console.ReadLine();
+
+        for (int i = 0; i < height; i++)
+        {
+
+        }
+    }
+}
+class Graph
+{
+    
+}
+class Lines
+{ 
+    
+}
+class Edge
+{
+    string Number;
+    int VisitedCount;
+
+    private Edge(string number)
+    {
+        this.Number = number;
+    }
+
+    public string Number1 { get => Number; set => Number = value; }
+    public int VisitedCount1 { get => VisitedCount; set => VisitedCount = value; }
+}

f04bf9a11b6f579c9312ae1d95665d439928281a
Semyon Lozhkin <SemLozhkin@gmail.com>
2022-09-29 15:05:40 +0500

    Сделал два массива. Первый - сам лабиринт. Второй - расстояния. Осталось поработать с очередью

--- ConsoleApp1/Detective Pikaptcha EP2.cs
+++ ConsoleApp1/Detective Pikaptcha EP2.cs
@@ -5,39 +5,51 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
+
 class Player
 {
-    static void Main()
+    static void Main(cell s, cell t)
     {
         string[] inputs = Console.ReadLine().Split(' ');
         int width = int.Parse(inputs[0]); //Ширина лабиринта
         int height = int.Parse(inputs[1]); //Длина лабиринта
-        bool[,] used = new bool[width,height]; //Массив посещённых вершин
-        string[] lines = new string[height];
-        int[,] labirint = new int[width,height];
-        int[] dst;
+
+        int[,] steps = { { +1, 0 }, { -1, 0 }, { 0, +1 }, { 0, -1 } };
+        
+        int[,] labirint = new int[width,height]; //Матрица лабиринта
+        int[,] distance = new int[width, height]; //Матрица дистанций от клетки s до остальных клеток
         char freeCell = '0';
-        for (int i = 0; i < height; i++)
+        
+        string[] lines = new string[height]; //Строчки, в которых вводится лабиринт
+        //Замена строк на массив неявного графа
+        for (int i = 0; i < height; i++) 
         {
             lines[i] = Console.ReadLine();
             for (int j = 0; j<width; j++)
             {
                 if (lines[i][j] == freeCell)
-                {
-                    labirint[i,j] = 0;
-                }
+                    labirint[i, j] = 0;
                 else
-                {
-                    labirint[i,j] = 1;
-                }
+                    labirint[i, j] = 1;
             }
+        }
+
+        Queue<cell> q = new Queue<cell>();
+        q.Enqueue(s); // Ввод начальной клетки в очередь
+
+        distance[s.x, s.y] = 0;
+
+        while (q.Count!=0)
+        {
+            //int[] [x, y] = { 1, 0 };
         }
         
 
         string side = Console.ReadLine();
     }
-    struct cell
-    {
-        int x, y;
-    }
+
+}
+public struct cell
+{
+    public int x, y;
 }
